5e346629ad0bdcf8a1dc77db0cf25f7d5f4d74d1
EthAnHoangg <an.hv204864@sis.hust.edu.vn>
2025-11-11 22:13:22 +1100

    Remove test.py file as it is no longer needed for the project. This file contained code for converting a PDF using the MarkItDown library.

--- test.py
+++
@@ -1,5 +0,0 @@
-from markitdown import MarkItDown
-
-md = MarkItDown(docintel_endpoint="https://docintel-endpoint.cognitiveservices.azure.com/")
-result = md.convert("test.pdf")
-print(result.text_content)
